16e5b883580228dbcafeb5f4139b3487019e25b7
Shekar Nyala <nshekarb4u@gmail.com>
2017-10-08 23:01:54 -0500

    Initial commit

---
+++ src/main/java/com/shekar/rest/SpringRestBackendApplication.java
@@ -0,0 +1,12 @@
+package com.shekar.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestBackendApplication.class, args);
+	}
+}
