e9e924bd1175cb37783096598045e100c60b49c9
Shekar Nyala <snyala@Virtusa.com>
2017-10-15 15:15:33 -0400

    additional acceptance setup

---
+++ src/test/java/com/shekar/rest/acceptance/util/AbstractSteps.java
@@ -0,0 +1,11 @@
+package com.shekar.rest.acceptance.util;
+
+import org.springframework.web.client.RestTemplate;
+
+public abstract class AbstractSteps {
+
+	protected static ResponseResults latestResponse = null;
+	protected RestTemplate restTemplate = null;
+	
+	
+}
